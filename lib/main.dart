b909d8f6b083033a3ba90aa89055ec7d19b2c3a5
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-10-13 14:25:06 +0330

    define app theme and implement it in material app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
+import 'package:ecommerce_zarinpal/ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const EcommerceApp());
+  runApp(EcommerceApp());
 }
 
 class EcommerceApp extends StatelessWidget {
-  const EcommerceApp({Key? key}) : super(key: key);
+  EcommerceApp({Key? key}) : super(key: key);
+
+  final AppTheme _appTheme = AppTheme();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
+      theme: _appTheme.theme(),
     );
   }
 }
---
+++ lib/ui/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData? theme() {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: 'Shabnam',
+    );
+  }
+}
