4d1a9d63dcf7d962eb744bd3c84ebc9170b3e10d
ayoubrajabi <ayoubrajabi72@gmail.com>
2021-10-13 14:18:08 +0330

    init project and add dependencies.

---
+++ android/app/src/main/kotlin/com/example/ecommerce_zarinpal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecommerce_zarinpal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcommerceApp());
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+    );
+  }
+}
